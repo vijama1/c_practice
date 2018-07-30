08e665ec082988c2cdf9b14ffedec6cc32d4daf0
Aman Vijay <amanvijay.cse19@jecrc.ac.in>
2018-07-30 22:56:03 +0530

    Palindrome

--- palindrome.C
+++ palindrome.C
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main()
 {
-  int n,same;
+  int n,same,num1,num2;
   int rem=0,rev=0;
-  printf("Enter the number");
+  int large=0,remainder1=0,small=2147483647;
+  printf("Enter the number:");
   scanf("%d",&n);
+  num1=n;
+  num2=n;
   same=n;
   while(n>0)
   {
@@ -14,11 +17,32 @@ int main()
   }
   if(same==rev)
   {
-    printf("\nNumber is palindrome");
+    printf("Number is palindrome\n");
+
+    while(num1>0)
+    {
+      remainder1=num1%10;
+      if(remainder1>large)
+      {
+      large=remainder1;
+    }
+      num1=int(num1/10);
+    }
+    printf("Largest number is %d ",large);
   }
   else
   {
-    printf("\nNumber is not palindrome");
+    printf("Number is not palindrome\n");
+    while(num2>0)
+    {
+      remainder1=num2%10;
+      if(remainder1<small)
+      {
+      small=remainder1;
+    }
+      num2=int(num2/10);
+    }
+    printf("Smallest number is %d ",small);
   }
   return 0;
 }
